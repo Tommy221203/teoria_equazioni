d21172b41d4d6437a35562aecb144bca249775fc
Tommy221203 <55913204+Tommy221203@users.noreply.github.com>
2019-12-19 20:57:04 +0100

    Add project files.

---
+++ EquazioniLibrary.Test/Equazioni_Test.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using EquazioniLibrary;
+
+namespace EquazioniLibrary.Test
+{
+    [TestClass]
+    public class Equazioni_Test
+    {
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+
+            double a = 3;
+            bool risposta_aspettata = true;
+            bool risposta = Equazioni.IsDetermined(a);
+            Assert.AreEqual(risposta_aspettata, risposta);
+        }
+            
+            [TestMethod]
+        public void TestMethod2()
+        {
+            double a = 3;
+            bool risposta2_aspettata = false;
+            bool risposta2 = Equazioni.IsInconsisted(a);
+            Assert.AreEqual(risposta2_aspettata, risposta2);
+            
+        }
+        [TestMethod]
+        public void TestMethod3()
+        {
+            double a = 3;
+            bool risposta_aspettata = false;
+            bool risposta = Equazioni.IsDegree2(a);
+            Assert.AreEqual(risposta_aspettata, risposta);
+        }
+        [TestMethod]
+        public void TestMethod4()
+        {
+            double a = 1;
+            double b = 3;
+            double c = -18;
+            double risposta_aspettata = 81;
+            double risposta = Equazioni.Delta(a,b,c);
+            Assert.AreEqual(risposta_aspettata, risposta);
+        }
+        public void TestMethod5()
+        {
+            double a = 2;
+            double b = 4;
+            double risposta_aspettata = -2;
+            double risposta = Equazioni.EquationDegree1(a, b);
+            Assert.AreEqual(risposta_aspettata, risposta);
+        }
+    }
+}
---
+++ EquazioniLibrary/Equazioni.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EquazioniLibrary
+{
+    public class Equazioni
+    {
+        public static bool IsDetermined(double a)
+        {
+            bool risposta = false;
+            if (a != 0)
+                risposta = true;
+            return risposta;
+        }
+        public static bool IsInconsisted(double a)
+        {
+            bool risposta2 = false;
+            if (a == 0)
+                risposta2 = true;
+            return risposta2;
+        }
+        public static bool IsDegree2(double a)
+        {
+            bool risposta3 = false;
+            if (a == 0)
+                risposta3 = true;
+            return risposta3;
+        }
+        public static double Delta(double a,double b, double c)
+        {
+            double delta = b * b - 4 * a * c;
+            return delta;
+        }
+        public static double EquationDegree1(double a, double b)
+        {
+            if(a==0&&b==0)
+            {
+                Console.WriteLine("indeterminato");
+            }
+            else if(a!=0&&b==0)
+            {
+                Console.WriteLine("impossibile");
+            }
+            else
+            {
+                double x = -b/a;
+                return x;
+            }
+            return 0;
+        }
+    }
+}
